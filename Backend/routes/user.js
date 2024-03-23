c5f03b2f66245568b210658e39322e05d747910a
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-03-23 16:10:49 +0530

    :sparkles:feat: update user and get or (filter) user routes

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -107,4 +107,60 @@ router.post("/signin", authMiddleware, async (req, res) => {
   }
 });
 
+//  Route to update user information
+const updateSchema = zod.object({
+  password: zod.string().optional(),
+  firstName: zod.string().optional(),
+  lastName: zod.string().optional(),
+});
+router.put("/", authMiddleware, async (req, res) => {
+  try {
+    const { success } = updateSchema.safeParse(req.body);
+    if (!success) {
+      res.status(411).json({
+        message: "Error while updating information",
+      });
+    }
+
+    await User.updateOne({ _id: req.userId }, req.body);
+
+    res.json({
+      message: "Updated successfully",
+    });
+  } catch (error) {
+    console.error("Error during sign-in:", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
+// Route to get users from the backend, filterable via firstName/lastName
+
+router.get("/getUser", async (req, res) => {
+  try {
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+      $or: [
+        { firstName: { $regex: filter, $options: "i" } }, // Case-insensitive regex match
+        { lastName: { $regex: filter, $options: "i" } }, // Case-insensitive regex match
+      ],
+    });
+
+    res.json({
+      users: users.map((user) => ({
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        _id: user._id,
+      })),
+    });
+  } catch (error) {
+    console.error("Error during fetching users:", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
+
 export default router;
