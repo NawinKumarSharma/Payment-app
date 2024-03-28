e727bb90ec583818f98518bc575b79676004171c
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-03-28 21:53:52 +0530

    :sparkles:feat: Dashboard connected to Backend

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,33 @@
-import { Balance } from "../components/Balance"
-import { Navbar } from "../components/Navbar"
-import { Users } from "../components/Users"
+import React, { useEffect, useState } from "react";
+import { Balance } from "../components/Balance";
+import { Navbar } from "../components/Navbar";
+import { Users } from "../components/Users";
+import axios from "axios";
 
 export const Dashboard = () => {
-    return <div>
-        <Navbar />
-        <div className="m-8">
-            <Balance value={"10,000"} />
-            <Users />
+    const [balance, setBalance] = useState(0);
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/v1/account/balance", {
+            headers: {
+                Authorization: "Bearer " + localStorage.getItem("token")
+            }
+        })
+        .then(response => {
+            setBalance(response.data.balance);
+        })
+        .catch(error => {
+            console.error('Error fetching balance:', error.message);
+        });
+    }, []);
+
+    return (
+        <div>
+            <Navbar />
+            <div className="m-8">
+                <Balance value={balance} /> 
+                <Users />
+            </div>
         </div>
-    </div>
-}
+    );
+};
