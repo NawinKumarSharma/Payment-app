6c9bf3cd1c10450813f8f6a28ed65bfcbdc42bab
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-03-28 21:54:55 +0530

    :sparkles:feat:display user to the dashboard

--- Frontend/src/components/Users.jsx
+++ Frontend/src/components/Users.jsx
@@ -1,43 +1,68 @@
-import { useState } from "react"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const Users = () => {
-    // Replace with backend call
-    const [users, setUsers] = useState([{
-        firstName: "Nawin",
-        lastName: "Sharma",
-        _id: 1
-    }]);
+    const [users, setUsers] = useState([]);
+    const [filter, setFilter] = useState("");
 
-    return <>
-        <div className="font-bold mt-6 text-lg">
-            Users
-        </div>
-        <div className="my-2">
-            <input type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200"></input>
-        </div>
-        <div>
-            {users.map(user => <User user={user} />)}
-        </div>
-    </>
-}
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/v1/user/getUser?filter=" + filter)
+            .then(response => {
+                setUsers(response.data.users);
+                // console.log(response.data.users)
+            })
+            .catch(error => {
+                console.error('Error fetching users:', error);
+            });
+    }, [filter]);
 
-function User({user}) {
-    return <div className="flex justify-between">
-        <div className="flex">
-            <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
-                <div className="flex flex-col justify-center h-full text-xl">
-                    {user.firstName[0]}
+    return (
+        <>
+            <div className="font-bold mt-6 text-lg">
+                Users
+            </div>
+            <div className="my-2">
+                <input
+                    onChange={(e) => setFilter(e.target.value)}
+                    type="text"
+                    placeholder="Search users..."
+                    className="w-full px-2 py-1 border rounded border-slate-200"
+                />
+            </div>
+            <div>
+                {users && users.map(user => <User key={user._id} user={user} />)}
+            </div>
+        </>
+    );
+};
+
+function User({ user }) {
+    const navigate = useNavigate();
+    // console.log(user);
+    return (
+        <div className="flex justify-between" key={user._id}>
+            <div className="flex">
+                <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
+                    <div className="flex flex-col justify-center h-full text-xl">
+                        {user.firstName[0]}
+                    </div>
+                </div>
+                <div className="flex flex-col justify-center h-ful">
+                    <div>
+                        {user.firstName} {user.lastName}
+                    </div>
                 </div>
             </div>
+
             <div className="flex flex-col justify-center h-ful">
-                <div>
-                    {user.firstName} {user.lastName}
-                </div>
+                <button
+                className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-800 hover:bg-green-900 text-white"
+                    onClick={() => {
+                        navigate("/send?id=" + user._id + "&name=" + user.firstName);
+                    }}
+                >Send money</button>
             </div>
         </div>
-
-        <div className="flex flex-col justify-center h-ful">
-            <button>Send money</button>
-        </div>
-    </div>
+    );
 }
